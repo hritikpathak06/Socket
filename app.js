1c37c79a3834e3dbcc3db33f13ad6f226ac0eae2
Ritik Kumar Pathak <phritik06@gmail.com>
2024-03-04 00:52:45 +0530

    changers

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:"https://socket-frontend-tau.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
